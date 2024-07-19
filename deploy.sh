c8bf3fae5a003a51b6fcce1c9cbb9ea7b514cd62
Nidhya R <163541752+Nidhyapandian@users.noreply.github.com>
2024-07-19 10:13:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 # GET CURRENT BRANCH:
-BRANCH_NAME = env.GIT_BRANCH.split('/').last()
+BRANCH_NAME=$(env.GIT_BRANCH.split('/').last())
 BRANCH_NAME=$(git rev-parse --abbrev -ref HEAD)
-
-
 if ["$BRANCH_NAME"="dev"]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
