5df477297a8f93994132f4f258aa1e720b434ec9
Nidhya R <163541752+Nidhyapandian@users.noreply.github.com>
2024-07-19 10:14:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,14 @@ if ["$BRANCH_NAME"="dev"]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     echo "haiiii dev"
-
     DOCKER_REPO="smart24/myapp-dev"
     docker tag myapp:${BUILD_NUMBER} $DOCKER_REPO:${BUILD_NUMBER}
     docker push $DOCKER_REPO:${BUILD_NUMBER}
     docker push $DOCKER_REPO:latest
-
 elif ["$BRANCH_NAME"="main"]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     echo "haiiii dev"
-    
     DOCKER_REPO="smart24/myapp-prod"
     docker tag myapp:${BUILD_NUMBER} $DOCKER_REPO:${BUILD_NUMBER}
     docker push $DOCKER_REPO:${BUILD_NUMBER}
